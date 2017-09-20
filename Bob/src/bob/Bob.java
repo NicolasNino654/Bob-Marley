6d1bb45cd50f9582f6f67a6c178b617cdb6d1cb3
Estudiantes <Estudiantes@Labing70203.udistrital.edu.co>
2017-09-20 17:31:24 -0500

    Proyecto completo

---
+++ Bob/src/bob/Bob.java
@@ -0,0 +1,10 @@
+package bob;
+
+
+public class Bob {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Bob Marly");
+        Rastas misGreñas = new Rastas();
+    }
+}
---
+++ Bob/src/bob/Rastas.java
@@ -0,0 +1,6 @@
+package bob;
+
+
+public class Rastas {
+    
+}
